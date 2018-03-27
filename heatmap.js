e698d4a3cdf33e27b646d874c648984494e2e262
Sahana Balram <sahana2008balram@gmail.com>
2018-03-27 14:35:34 -0700

    Declare the scale

--- heatmap.js
+++ heatmap.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
     const url="https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+
+    d3.json(url,function(data){
+        data = data.monthlyVariance;
+        data.map(function(d){
+            d.month = monthes[d.month-1];
+            d.year = d3.time.format("%Y").parse(d.year.toString());
+        });
+
+        // declare the scale
+
+        xScale.domain(d3.extend(data,function(data){
+            return data.year;
+        }));
+
+        colorScale.domain(d3.extend(data,function(d){
+            return d.variance;
+        }));
+    })
 });
